3c6fddc36adf2233bb7bf4e9dacb3da73303391f
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-03 16:35:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,10 @@ class Song
   @@genre_count = []
   @@artist_count = []
   
-  def initialize
-    @name
-    @artist
-    @genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << :genre
     @@artists << :artist
