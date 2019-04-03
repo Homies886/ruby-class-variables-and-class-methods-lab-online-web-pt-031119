2976f18d3e18b2eddb5b2301be90b401f09cafb9
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-03 18:01:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.genre_count
     count = 0
     @@genres.each do |genre|
-      @@genre_count[genre] += count
+      @@genre_count[genre] += (count += 1)
     end  
     
   end
